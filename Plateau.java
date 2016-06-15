de19251b9aeca5cdc9c76baa9fd7048f78723409
ZpXx <nic.ruggiero@gmail.com>
2016-06-15 14:37:37 +0200

    Add files via upload

---
+++ Plateau.java
@@ -0,0 +1,9 @@
+public abstract class Plateau extends JPanel
+{
+	//Attributs
+	protected int larg;
+	protected int longu;
+	protected int form_case;
+	protected int form_plat;
+	protected Pion [] pion = new Pion[larg][longu]; 
+}

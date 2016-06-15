385afc5d4a41829e0392f2c679ec9e5950a39798
94810 <pablo.bourdelas@gmail.com>
2016-06-15 17:15:29 +0200

    extends for hexagon

--- Plateau.java
+++ Plateau.java
@@ -1,9 +1,9 @@
 public abstract class Plateau
 {
 	//Attributs
-	protected int larg;
-	protected int longu;
-	protected int form_case;
-	protected int form_plat;
-	protected Pion [] pion = new Pion[larg][longu]; 
+	protected int larg ;
+	protected int longu ;
+	protected int form_case ;
+	protected int form_plat ;
+	protected Pion[][] pion ; 
 }
